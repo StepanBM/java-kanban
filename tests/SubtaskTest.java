bd0e1e7703dbcdd2453370d7fee41d88601b2fd5
Stepan Belenov <Sabaneev99@yandex.ru>
2025-03-12 22:15:58 +0300

    Правки 2

---
+++ tests/SubtaskTest.java
@@ -0,0 +1,56 @@
+
+
+import manager.HistoryManager;
+import manager.Managers;
+import manager.TaskManager;
+import org.junit.jupiter.api.Test;
+import status.TaskStatus;
+import tasks.Epic;
+import tasks.Subtask;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SubtaskTest {
+
+    HistoryManager historyManager = Managers.getDefaultHistory();
+    TaskManager taskManager = Managers.getDefault(historyManager);
+
+    Epic epic1 = new Epic("Имя №1", "Ооооочень длинное описание № 1", TaskStatus.NEW);
+    final int epicId = taskManager.createEpic(epic1);
+
+    @Test
+    void addNewSubtask() {
+
+        Subtask subtask = new Subtask("Имя подзадачи №1", "Ооооочень длинное описание № 1", TaskStatus.NEW);
+        Subtask subtask2 = new Subtask("Имя подзадачи №2", "Ооооочень длинное описание № 2", TaskStatus.NEW);
+        Subtask subtask3 = new Subtask("Имя подзадачи №3", "Ооооочень длинное описание № 3", TaskStatus.NEW);
+        final int subtaskId = taskManager.createSubtask(epicId, subtask);
+        final int subtaskId2 = taskManager.createSubtask(epicId, subtask2);
+        final int subtaskId3 = taskManager.createSubtask(epicId, subtask3);
+
+        final Subtask savedSubtask = taskManager.getByIdSubtask(subtaskId);
+
+        assertEquals(subtask, savedSubtask, "Задачи не совпадают.");
+        assertNotNull(savedSubtask, "Задача не найдена.");
+
+        final List<Subtask> subtasks = taskManager.outputAllSubtask();
+
+        assertNotNull(subtasks, "Задачи не возвращаются.");
+        assertEquals(3, subtasks.size(), "Неверное количество задач.");
+        assertEquals(subtask, subtasks.get(0), "Задачи не совпадают.");
+    }
+
+    @Test
+    void checkingForSubtaskConflictWithGivenIdAndGeneratedId() {
+
+        Subtask subtask1 = new Subtask("Имя №1", "Ооооочень длинное описание № 1", TaskStatus.NEW);
+        Subtask subtask2 = new Subtask("Имя №1", "Ооооочень длинное описание № 1", TaskStatus.NEW);
+        final int subtaskId = taskManager.createSubtask(epicId, subtask1);
+
+        Subtask savedSubtask = taskManager.updateSubtask(subtaskId, subtask2);
+        assertEquals(subtask1, savedSubtask, "Задачи не совпадают.");
+    }
+
+}
---
+++ tests/TaskTest.java
@@ -0,0 +1,48 @@
+
+
+import manager.HistoryManager;
+import manager.Managers;
+import manager.TaskManager;
+import org.junit.jupiter.api.Test;
+import status.TaskStatus;
+import tasks.Task;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+
+    HistoryManager historyManager = Managers.getDefaultHistory();
+    TaskManager taskManager = Managers.getDefault(historyManager);
+
+    @Test
+    void addNewTask() {
+
+        Task task = new Task("Имя №1", "Ооооочень длинное описание № 1", TaskStatus.NEW);
+        final int taskId = taskManager.createTask(task);
+
+        final Task savedTask = taskManager.getByIdTask(taskId);
+
+        assertEquals(task, savedTask, "Задачи не совпадают.");
+        assertNotNull(savedTask, "Задача не найдена.");
+
+        final List<Task> tasks = taskManager.outputAllTask();
+
+        assertNotNull(tasks, "Задачи не возвращаются.");
+        assertEquals(1, tasks.size(), "Неверное количество задач.");
+        assertEquals(task, tasks.get(0), "Задачи не совпадают.");
+    }
+
+    @Test
+    void checkingForTaskConflictWithGivenIdAndGeneratedId() {
+
+        Task task1 = new Task("Имя №1", "Ооооочень длинное описание № 1", TaskStatus.NEW);
+        Task task2 = new Task("Имя №1", "Ооооочень длинное описание № 1", TaskStatus.NEW);
+        final int taskId = taskManager.createTask(task1);
+
+        Task savedTask = taskManager.updateTask(taskId, task2);
+        assertEquals(task1, savedTask, "Задачи не совпадают.");
+    }
+
+}
